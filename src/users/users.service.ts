4cb872da5b731d8fe475ee85eef8c60cabdd77e1
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-01 11:47:29 -0700

    Guardar usuario que actualizó el registro

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -65,9 +65,10 @@ export class UsersService {
     }
   }
 
-  async block(id: string): Promise<User> {
+  async block(id: string, adminUser: User): Promise<User> {
     const userToBlock = await this.findOneById(id);
     userToBlock.isActive = false;
+    userToBlock.lastUpdateBy = adminUser;
     return await this.usersRepository.save(userToBlock);
   }
 
