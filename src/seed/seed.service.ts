f0f9b40e00e715a99799ad6ae1a4ad607026dc9c
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-06 19:40:46 -0700

    Seed - Paso 2: Borrar registros anteriores

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,11 +1,22 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Item } from '../items/entities/item.entity';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class SeedService {
   private isProd: boolean;
 
-  constructor(private readonly configService: ConfigService) {
+  constructor(
+    private readonly configService: ConfigService,
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>,
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {
     this.isProd = configService.get('STATE') === 'prod';
   }
 
@@ -13,11 +24,23 @@ export class SeedService {
     if (this.isProd)
       throw new UnauthorizedException('Seed cannot be executed on PRD');
 
-    // TODO: Limpiar la base de datos
-
+    await this.deleteDatabase();
     // TODO: Crear usuarios
 
     // TODO: Crear items
     return true;
+  }
+
+  async deleteDatabase() {
+    await this.itemsRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
+    await this.usersRepository
+      .createQueryBuilder()
+      .delete()
+      .where({})
+      .execute();
   }
 }
