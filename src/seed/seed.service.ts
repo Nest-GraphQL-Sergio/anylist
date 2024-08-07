f41c3cbea18080c76a76dcd5f44dc233ab3035a6
Sergio Barreras <sergiobg.isc@gmail.com>
2024-08-06 20:26:02 -0700

    Seed - Paso 3: Crear usuarios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -3,8 +3,12 @@ import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
+import { UsersService } from '../users/users.service';
+import { ItemsService } from '../items/items.service';
+
 import { Item } from '../items/entities/item.entity';
 import { User } from '../users/entities/user.entity';
+import { SEED_USERS } from './data/seed-data';
 
 @Injectable()
 export class SeedService {
@@ -12,6 +16,8 @@ export class SeedService {
 
   constructor(
     private readonly configService: ConfigService,
+    private readonly usersService: UsersService,
+    private readonly itemService: ItemsService,
     @InjectRepository(Item)
     private readonly itemsRepository: Repository<Item>,
     @InjectRepository(User)
@@ -25,13 +31,13 @@ export class SeedService {
       throw new UnauthorizedException('Seed cannot be executed on PRD');
 
     await this.deleteDatabase();
-    // TODO: Crear usuarios
+    const user = await this.loadUsers();
 
     // TODO: Crear items
     return true;
   }
 
-  async deleteDatabase() {
+  async deleteDatabase(): Promise<void> {
     await this.itemsRepository
       .createQueryBuilder()
       .delete()
@@ -42,5 +48,15 @@ export class SeedService {
       .delete()
       .where({})
       .execute();
+  }
+
+  async loadUsers(): Promise<User> {
+    const users = [];
+
+    for (const user of SEED_USERS) {
+      users.push(await this.usersService.create(user));
+    }
+
+    return users[0];
   }
 }
